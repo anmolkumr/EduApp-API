1d6a5d3dee28413c9a2da6e9d56c3b5ffff6ebe0
Anmol Kumar <77740197+anmolkumr@users.noreply.github.com>
2023-12-29 18:23:08 +0530

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
-
+app.use(cors());
 // Connect to MongoDB
 mongoose.connect('mongodb+srv://anmol4979199:anmol123@test.8oysegr.mongodb.net/scertbooks', { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
